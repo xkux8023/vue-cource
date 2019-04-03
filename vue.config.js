6f1427da4be19982d7e7174c88ec29c63773ac70
xkux8023 <326712928@qq.com>
2019-04-03 21:23:56 +0800

    项目工程初始化

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,18 @@
+const path = require('path')
+
+const resolve = dir => path.join(__dirname, dir)
+const BASE_URL = process.env.NODE_ENV === 'prodection' ? '/iview-admin' : '/'
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  baseUrl: BASE_URL,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('_c', resolve('src/components'))
+  },
+  productionSourceMap: false,
+  devServer: {
+    proxy: 'http://location:4000'
+  }
 }
