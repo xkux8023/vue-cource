face012d183509126ea25a099285b8da9ff40adf
xkux8023 <326712928@qq.com>
2019-04-05 17:59:54 +0800

    axios封装

---
+++ src/lib/axios.js
@@ -0,0 +1,50 @@
+import axios from 'axios'
+import { baseURL } from '@/config'
+class HttpRequest {
+  constructor (baseUrl = baseURL) {
+    this.baseUrl = baseUrl
+    this.queue = {}
+  }
+  getInsideConfig () {
+    const config = {
+      baseURL: this.baseUrl,
+      headers: {
+        //
+      }
+    }
+    return config
+  }
+  distroy (url) {
+    delete this.queue[url]
+    if (!Object.keys(this.queue).length) {
+      // Spin.hide()
+    }
+  }
+  interceptors (instance, url) {
+    instance.interceptors.request.use(config => {
+      // 添加全局的loading...
+      if (!Object.keys(this.queue).length) {
+        // Spin.show()
+      }
+      this.queue[url] = true
+      return config
+    }, error => {
+      return Promise.reject(error)
+    })
+    instance.interceptors.response.use(res => {
+      this.distroy(url)
+      const { data, status } = res
+      return { data, status }
+    }, error => {
+      this.distroy(url)
+      return Promise.reject(error)
+    })
+  }
+  request (options) {
+    const instance = axios.create()
+    options = Object.assign(this.getInsideConfig(), options)
+    this.interceptors(instance, options.url)
+    return instance(options)
+  }
+}
+export default HttpRequest
