2110b4981bee1d485101fdc7384990a781781185
xkux8023 <326712928@qq.com>
2019-04-05 17:44:01 +0800

    vux-进阶(插件功能与严格模式）

--- src/store/index.js
+++ src/store/index.js
@@ -5,15 +5,18 @@ import mutations from './mutations'
 import state from './state'
 import getters from './getters'
 import user from './module/user'
+import saveInLocal from './plugin/saveInLocal'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  strict: process.env.NODE_ENV === 'development',
   state,
   getters,
   mutations,
   actions,
   modules: {
     user
-  }
+  },
+  plugins: [ saveInLocal ]
 })
